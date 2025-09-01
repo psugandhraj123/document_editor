032717237457198c2008acf902410300e50af8a4
Sugandhraj Patel <psugandhraj@gmail.com>
2025-09-02 05:28:59 +0530

    build error fixed

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, type Action, type ThunkAction } from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
 import { rootReducer } from "./slice";
 
 export const store = configureStore({
@@ -9,10 +9,3 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-// ✅ Define AppThunk for type-safe thunks
-// export type AppThunk<ReturnType = void> = ThunkAction<
-//   ReturnType,
-//   RootState,
-//   unknown,
-//   Action<string>
-// >;
