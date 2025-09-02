666d1b75e552359075fffebd2304df723727ce84
Sugandhraj Patel <psugandhraj@gmail.com>
2025-09-02 05:56:02 +0530

    dev tools removed

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -4,7 +4,6 @@ import { rootReducer } from "./slice";
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefault) => getDefault(),
-  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export type RootState = ReturnType<typeof store.getState>;
